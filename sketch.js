6ab6aa33d9f204494c340ebb0b9077e01220329b
lainrr <61434548+lainrr@users.noreply.github.com>
2020-08-20 16:00:16 +0100

    update

---
+++ sketch.js
@@ -0,0 +1,38 @@
+const shakeButton = document.querySelector('shake');
+
+
+const answers = ['yes.', 'no.', 'maybe?'];
+let answer = ''
+
+shakeButton.onClick = () => {
+  answerIndex = Math.floor(Math.random() * 2);
+  answer = answers[answerIndex];
+  console.log(answer);
+}
+
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    noStroke();
+    fill('#2D5D7B');
+    ellipse(210, 210, 300);
+    fill('#457EAC');
+    ellipse(200, 200, 300);
+    
+    stroke('#E7C5C5');
+    strokeWeight(2);
+    strokeJoin(ROUND);
+    fill('#F1DEDE');
+    triangle(200, 105, 120, 255, 280, 255);
+    
+    noStroke();
+    fill('#2D5D7B');
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text(answer, width/2, height/2);
+    
+    
+  }
