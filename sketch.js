90b80a24398ef0cd60a65448cc5e64f1be9cff4c
lainrr <61434548+lainrr@users.noreply.github.com>
2020-08-20 22:08:19 +0100

    updated color and copy

--- sketch.js
+++ sketch.js
@@ -5,13 +5,31 @@ const question = document.getElementById('question');
 const answers = ['yes.', 'no!', 'maybe?', 'defo not', 'probs not', 'for sure', 'idk'];
 let answer = ''
 
-function pickRandomAnswer () {
-  answerIndex = Math.floor(Math.random() * 7);
-  answer = answers[answerIndex];
-}
+function shake () {
+  if (shakeButton.innerHTML === 'Shake!') {
+    answerIndex = Math.floor(Math.random() * 7);
+    answer = answers[answerIndex];
+    shakeButton.innerHTML = 'Shake again?'
+    question.style.backgroundColor = '#cbf3d2';
+    question.style.border = '2px solid white';
+  } else {
+    answer = '';
+    question.value = '';
+    shakeButton.innerHTML = 'Shake!'
+    question.style.backgroundColor = 'white';
+    question.style.border = '2px solid #9CE8AA';
+  }
+};
 
-shakeButton.addEventListener('click', pickRandomAnswer);
+shakeButton.addEventListener('click', shake);
 
+/*
+const maroon = '#330c2f';
+const purp = '#7b287d';
+const lilia = '#7067cf';
+const ltPurple = '#b7c0ee';
+const gree = '#cbf3d2';
+*/
 
 function setup() {
     createCanvas(400, 400);
@@ -19,19 +37,19 @@ function setup() {
   
   function draw() {
     noStroke();
-    fill('#2D5D7B');
+    fill('#330c2f');
     ellipse(210, 210, 300);
-    fill('#457EAC');
+    fill('#7067cf');
     ellipse(200, 200, 300);
     
-    stroke('#E7C5C5');
-    strokeWeight(2);
-    strokeJoin(ROUND);
-    fill('#F1DEDE');
+    stroke('#330c2f');
+    strokeWeight(5);
+    //strokeJoin(ROUND);
+    fill('#9CE8AA');
     triangle(200, 105, 120, 255, 280, 255);
     
     noStroke();
-    fill('#2D5D7B');
+    fill('#7067cf');
     textSize(20);
     textAlign(CENTER, CENTER);
     text(answer, width/2, height/2);
